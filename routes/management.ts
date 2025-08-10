4fa29b2262d186c237ba396f45a23c515953bf3a
hamashua1 <joshuahamashua@gmail.com>
2025-08-10 02:55:53 +0000

    file updated

--- routes/management.ts
+++ routes/management.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 const router = express.Router()
-import {readUser , readSpecificUser, updateUser} from '../controllers/managementController'
+import {readUser , readSpecificUser, updateUser} from '../controllers/managementController.ts'
 
 
 // user endpoints
