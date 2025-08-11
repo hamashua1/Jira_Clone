3ccc06700d661bb6084f94f89c296fee41e64c9a
hamashua1 <joshuahamashua@gmail.com>
2025-08-11 16:40:02 +0000

    file updated

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,11 +1,11 @@
 import express from 'express'
 const router = express.Router()
-import {adminRegister, adminSignin, sign_in} from '../controllers/loginController.ts'
+import {adminRegister, adminSignin, sign_in, userRegister} from '../controllers/loginController.ts'
 
 router.post('/api/task/register',adminRegister)
 router.post('/api/task/sign_in', sign_in)
 router.post('/api/adminSignin', adminSignin)
-
+router.post('/api/task/userRegister',userRegister)
 
 
 export default router
